9765fb567ea097fbc9dc93a24a0f4d785ad69fb4
Miguel Angel Coca <redridehell@gmail.com>
2018-10-08 11:44:05 -0400

    adding fluent example

---
+++ src/test/java/com/truextend/PageObjects/GoogleHomePage.java
@@ -0,0 +1,45 @@
+package com.truextend.PageObjects;
+
+import com.lazerycode.selenium.util.Query;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+public class GoogleHomePage extends BasePage {
+
+    private Query searchBar = new Query(By.name("q"), driver);
+    private Query googleSearch = new Query(By.name("btnK"), driver);
+    private Query imFeelingLucky = new Query(By.name("btnI"), driver);
+
+    public GoogleHomePage() { }
+
+    public GoogleHomePage SelectSearchBox(){
+        searchBar.findWebElement().click();
+        return this;
+    }
+
+    public GoogleHomePage TypeIn(String searchTerm) {
+        searchBar.findWebElement().clear();
+        searchBar.findWebElement().sendKeys(searchTerm);
+        return this;
+    }
+
+    public GoogleHomePage HitEnter() {
+        googleSearch.findWebElement().submit();
+        return this;
+    }
+
+    public GoogleHomePage NavigateTo(String url){
+        driver.get(url);
+        return this;
+    }
+
+    public GoogleHomePage VerifyResultsPageTitleContains(String expected){
+        Assert.assertTrue(driver.getTitle().contains(expected));
+        return this;
+    }
+
+    public void clickGetLucky() {
+        imFeelingLucky.findWebElement().click();
+    }
+
+}
---
+++ src/test/java/com/truextend/Tests/FluentSearchTests.java
@@ -0,0 +1,29 @@
+package com.truextend.Tests;
+
+import com.truextend.Listeners.ScreenshotListener;
+import com.truextend.PageObjects.GoogleHomePage;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+@Listeners(ScreenshotListener.class)
+public class FluentSearchTests extends TestBase {
+
+    GoogleHomePage searchEngine;
+
+    @BeforeClass
+    public void setup(){
+        searchEngine = new GoogleHomePage();
+    }
+
+    @Test
+    public void fluentGoogleCheeseSearch() throws Exception{
+
+        searchEngine.NavigateTo("http://www.google.com")
+                .SelectSearchBox()
+                .TypeIn("Cheese!")
+                .HitEnter()
+                .VerifyResultsPageTitleContains("Cheese");
+    }
+
+}
